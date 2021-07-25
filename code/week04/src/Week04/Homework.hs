662a8184cfa1b50323d973b05b55d98974cffb5e
aimfeld <adrian.imfeld@brightanswer.ch>
2021-07-25 13:07:39 +0200

    Homework week04

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -9,13 +9,15 @@ module Week04.Homework where
 
 import Data.Aeson            (FromJSON, ToJSON)
 import Data.Functor          (void)
-import Data.Text             (Text)
+import Data.Text             (Text, unpack)
 import GHC.Generics          (Generic)
 import Ledger
 import Ledger.Ada            as Ada
 import Ledger.Constraints    as Constraints
 import Plutus.Contract       as Contract
 import Plutus.Trace.Emulator as Emulator
+import Wallet.Emulator.Wallet
+import Control.Monad.Freer.Extras as Extras
 
 data PayParams = PayParams
     { ppRecipient :: PubKeyHash
@@ -28,14 +30,30 @@ payContract :: Contract () PaySchema Text ()
 payContract = do
     pp <- endpoint @"pay"
     let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
-    void $ submitTx tx
+    Contract.handleError 
+        (\err -> Contract.logError $ "caught: " ++ unpack err)
+        (void $ submitTx tx)
     payContract
 
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
 -- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
-payTrace _ _ = undefined -- IMPLEMENT ME!
+payTrace payAmount1 payAmount2 = 
+    do
+        h1 <- activateContractWallet (Wallet 1) payContract
+
+        callEndpoint @"pay" h1 $ payWallet2Params payAmount1
+        void $ Emulator.waitNSlots 1
+
+        callEndpoint @"pay" h1 $ payWallet2Params payAmount2
+        void $ Emulator.waitNSlots 1
+        Extras.logInfo @String $ "Trace completed."
+    where
+        payWallet2Params amount = PayParams
+            { ppRecipient = pubKeyHash $ walletPubKey $ Wallet 2
+            , ppLovelace  = amount
+            }
 
 payTest1 :: IO ()
 payTest1 = runEmulatorTraceIO $ payTrace 1000000 2000000
